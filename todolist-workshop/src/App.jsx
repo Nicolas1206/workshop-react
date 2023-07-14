934c3420ab4c7768e03b219c591fe4518d95128a
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-07-14 09:33:03 +0200

    chore: début projet

---
+++ todolist-workshop/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  
+
+  return (
+   
+    <main className='container'>
+      <h1>hello world</h1>
+      </main>
+    
+  )
+}
+
+export default App
