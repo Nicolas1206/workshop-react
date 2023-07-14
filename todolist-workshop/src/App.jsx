18e16b768a94ed24fdf37bdb7df729974c3596f4
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-07-14 11:51:17 +0200

    lift the state up

--- todolist-workshop/src/App.jsx
+++ todolist-workshop/src/App.jsx
@@ -3,7 +3,39 @@ import Header from './components/Header.jsx';
 import AddTodo from './components/AddTodo.jsx'
 import ListTodos from './components/ListTodos.jsx';
 function App() {
-  
+//   const handleClick = (e) => {
+//     const newTodos = [...todos];
+//     newTodos.push("This is a new todo");
+//     setTodos(newTodos);
+// }
+const initTodos = [
+  {
+  id: "1",
+  name: "Apprendre React",
+  done: false
+},
+{
+  id: "2",
+  name: "Faire la vaisselle",
+  done: false
+} ,
+{
+  id: "3",
+  name: "Faire un jogging",
+  done: false
+} 
+];
+const [todos, setTodos] = useState(initTodos);
+
+const changeTodoStatus = (id) => {
+  const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+          todo.done = !todo.done;
+      }
+      return todo;
+  });
+  setTodos(newTodos);
+}
 
   return (
    
@@ -12,7 +44,7 @@ function App() {
 
          <AddTodo />
 
-         <ListTodos />
+         <ListTodos todos={todos} changeTodoStatus={changeTodoStatus}/>
 
       </main>
     
--- todolist-workshop/src/components/ListTodos.jsx
+++ todolist-workshop/src/components/ListTodos.jsx
@@ -1,40 +1,20 @@
 import { useState } from "react";
 
-export default function ListTodos() {
-    const initTodos = [
-        {
-        id: "1",
-        name: "Apprendre React",
-        done: false
-    },
-    {
-        id: "2",
-        name: "Faire la vaisselle",
-        done: false
-    } ,
-    {
-        id: "3",
-        name: "Faire un jogging",
-        done: false
-    } 
-];
-    const [todos, setTodos] = useState(initTodos);
-    const handleCheckboxClick = (e) => {
-        const newTodos = todos.map((todo) => {
-            if (todo.id === e.target.id){
-                todo.done = !todo.done;
-            }
-            return todo;
-        });
-        setTodos(newTodos);
-    }
+export default function ListTodos({todos, changeTodoStatus}) {
+    
+    
     return (
         <section>
         <h2>Todos</h2>
         <ul>
             {todos.map((todo, index) => (
             <li key={index}>
-            <input type="checkbox" id={todo.id} checked={todo.done} onChange={handleCheckboxClick}/> 
+            <input 
+            type="checkbox" 
+            id={todo.id} 
+            checked={todo.done} 
+            onChange={ () => {changeTodoStatus(todo.id)}}
+            /> 
             {todo.name}
             </li>
             ))}
