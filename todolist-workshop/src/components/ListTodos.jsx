63c9481cc97b99f1da7f26ee42dace35b6bd9200
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-07-14 10:58:46 +0200

    chore: step2

--- todolist-workshop/src/components/ListTodos.jsx
+++ todolist-workshop/src/components/ListTodos.jsx
@@ -1,21 +1,45 @@
+import { useState } from "react";
+
 export default function ListTodos() {
+    const initTodos = [
+        {
+        id: "1",
+        name: "Apprendre React",
+        done: false
+    },
+    {
+        id: "2",
+        name: "Faire la vaisselle",
+        done: false
+    } ,
+    {
+        id: "3",
+        name: "Faire un jogging",
+        done: false
+    } 
+];
+    const [todos, setTodos] = useState(initTodos);
+    const handleCheckboxClick = (e) => {
+        const newTodos = todos.map((todo) => {
+            if (todo.id === e.target.id){
+                todo.done = !todo.done;
+            }
+            return todo;
+        });
+        setTodos(newTodos);
+    }
     return (
         <section>
         <h2>Todos</h2>
         <ul>
-          <li>
-          <input type="checkbox" />
-            Apprendre React
-          </li>
-          <li>
-          <input type="checkbox" />
-            Faire la vaisselle
-          </li>
-          <li>
-          <input type="checkbox" />
-            Faire un jogging
-          </li>
+            {todos.map((todo, index) => (
+            <li key={index}>
+            <input type="checkbox" id={todo.id} checked={todo.done} onChange={handleCheckboxClick}/> 
+            {todo.name}
+            </li>
+            ))}
         </ul>
+        {/* <button onClick={handleClick}>Add a todo</button> */}
         </section>
     );
 }
